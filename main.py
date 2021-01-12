a0def872ff5a6b2cbfdfc2acf6154e3fef88d7eb
John Ronis <jronis@gmail.com>
2021-01-12 17:46:00 +1000

    small change to main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+import pandas
 
 screen = Screen()
 screen.title("US States Game")
@@ -7,9 +8,16 @@ screen.addshape(image)
 
 turtle = Turtle()
 turtle.shape(image)
+state_df = pandas.read_csv("50_states.csv")
 
 answer_state = screen.textinput(
     title="Guess the State", prompt="What's another state name?"
 )
+
+
+def get_coords(state):
+    pass
+
+
 print(answer_state)
 screen.mainloop()
